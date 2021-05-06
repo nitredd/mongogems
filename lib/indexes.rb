fb74481449c55f5e63458bc16c90e88bc54b0423
Katkam Nitin Narsing Reddy <nitredd@gmail.com>
2021-05-06 23:38:15 +0400

    Write MongoDB indexes to file with mongoindex2file

---
+++ lib/indexes.rb
@@ -0,0 +1,66 @@
+require 'mongo'
+require 'json'
+
+def get_indexes_script(uri, db_name=nil, coll_name=nil, outfile)
+  client = Mongo::Client.new(uri)
+
+
+  db_list = []
+  if not db_name.nil?
+    db_list.push db_name
+  else
+    client.use('admin').command(listDatabases: 1).first['databases'].each do |iter|  # read the name property
+      iter_db_name = iter['name']
+      db_list.push iter_db_name
+    end
+  end
+
+  # client = client.use 'test'
+
+  # cur = client.command listCollections: 1
+  # rset = cur.to_a
+  # rset = rset.first['cursor']['firstBatch']
+  # rset
+
+  scripts = []
+
+  db_list.each do |iter_db|
+    client = client.use iter_db
+    scripts.push "use #{iter_db};"
+    if coll_name.nil?
+      colls = client.collections
+    else
+      colls = [coll_name]
+    end
+
+    colls.each do |coll|
+      coll.indexes.each do |idx|
+        txt = gen_idx_script idx, coll.name
+        scripts.push txt
+      end
+    end
+  end
+
+  txtscripts = scripts.join "\r\n"
+
+  File.open outfile, 'w' do |f|
+    f.write txtscripts
+  end
+end
+
+def gen_idx_script(idIndex, collname)
+  idx_name = idIndex['name']
+  idx_spec = JSON.dump(idIndex['key'])
+  idx_options = {}
+  idIndex.each do |k, v|
+    if ['v', 'key'].include? k then
+      next
+    end
+    idx_options[k] = v
+  end
+  opt_spec = JSON.dump idx_options
+  txt = "db.#{collname}.createIndex(#{idx_spec}, #{opt_spec});"
+end
+
+# script = (get_indexes_script 'mongodb://localhost').join "\n"
+# puts script
--- mongogems.gemspec
+++ mongogems.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |spec|
   spec.authors = ["Katkam Nitin Reddy"]
   spec.name = "mongogems"
-  spec.version = "0.0.3"
-  spec.date = '2021-05-04'
+  spec.version = "0.0.4"
+  spec.date = '2021-05-06'
   spec.summary = "MongoDB Utilities"
-  spec.files = ['lib/savedPipeline.rb', 'lib/logs.rb']
+  spec.files = ['lib/savedPipeline.rb', 'lib/logs.rb', 'lib/indexes.rb']
   spec.require_paths = ["lib"]
-  spec.executables=['compass2mongosh', 'mongolog2file']
+  spec.executables=['compass2mongosh', 'mongolog2file', 'mongoindex2file']
   spec.add_dependency "mongo", "~> 2.14.0"
 end
